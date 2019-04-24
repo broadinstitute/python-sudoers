3fa5f9c35e58b6fb518b793708f601c8b341264a
Andrew Teixeira <teixeira@broadinstitute.org>
2019-04-24 15:07:21 -0400

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_long_description():
 
 setuptools.setup(
     name="pysudoers",
-    version="0.1.0",
+    version="1.0.0",
     author="Andrew Teixeira",
     author_email="teixeira@broadinstitute.org",
     description="Python interface to the Linux sudoers file",
