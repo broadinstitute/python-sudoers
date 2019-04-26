ee8d4b4f7fee2ef130942353e31a3dace9aac7c2
Andrew Teixeira <teixeira@broadinstitute.org>
2019-04-26 15:13:58 -0400

    Bump version: 1.0.1 → 1.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "1.0.1"
+VERSION = "1.1.0"
 
 
 def get_long_description():
